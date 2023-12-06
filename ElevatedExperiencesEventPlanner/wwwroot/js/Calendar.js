96f7698b4034912bc2ebecf60010b081b4f3de9d
danielchoike2 <choike3@mail.nmc.edu>
2023-12-06 18:30:52 -0500

    Add project files.

---
+++ ElevatedExperiencesEventPlanner/wwwroot/js/Calendar.js
@@ -0,0 +1,19 @@
+﻿let calendarE1 = document.getElementById('calendar');
+
+let calendar = new FullCalendar.Calendar(calendarE1, {
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+    events: [
+        {
+            title: "Record a video for Marisa",
+            start: "2023-12-5"
+        },
+    ],
+
+});
+
+calendar.render();
